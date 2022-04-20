a33de0e4ec412bda83a2b398d61bdf925ab33764
jmiguel1963 <svsjmvsti@gmail.com>
2022-04-20 13:33:05 +0200

    Multitouch_first_commit
    Commit initial files to github

---
+++ MultitouchGame/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    MultitouchView myTouchView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myTouchView = new MultitouchView(this);
+        setContentView(myTouchView);
+        //setContentView(R.layout.activity_main);
+    }
+}
---
+++ MultitouchGame/app/src/main/java/com/example/myapplication/MultitouchView.java
@@ -0,0 +1,93 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MultitouchView extends View implements View.OnTouchListener{
+
+    private Paint paint;
+    private int[] colors={Color.BLACK,Color.BLUE,Color.YELLOW,Color.GREEN,Color.CYAN,Color.RED};
+    private CountDownTimer cTimer = null;
+    private boolean selectedPlayer=false;
+
+    Map <Integer,Vector2D> map =  new HashMap<Integer,Vector2D>();
+    public MultitouchView(Context context) {
+        super(context);
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setARGB(255,255,0,0);
+
+        this.setOnTouchListener(this);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        int index=0;
+        for (Map.Entry<Integer, Vector2D> pair: map.entrySet()) {
+            int x = pair.getValue().GetPosX();
+            int y = pair.getValue().GetPosY();
+            paint.setColor(colors[index%colors.length]);
+            canvas.drawCircle(x,y,100, paint);
+            index++;
+        }
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event){
+
+        int pointerIndex = event.getActionIndex();
+        int pointerID = event.getPointerId(pointerIndex);
+        Log.i("distance",""+pointerID);
+        int maskedAction = event.getActionMasked();
+
+        switch (maskedAction){
+            case MotionEvent.ACTION_POINTER_DOWN:
+            case MotionEvent.ACTION_DOWN: {
+                int x = (int) event.getX(pointerIndex);
+                int y = (int) event.getY(pointerIndex);
+                Vector2D newPoint = new Vector2D(x, y);
+                map.put(pointerID, newPoint);
+            }
+
+            break;
+            case MotionEvent.ACTION_MOVE:{
+                int size = event.getPointerCount();
+                for (int i = 0; i < size; i++) {
+                    Vector2D point = map.get(event.getPointerId(i));
+                    if (point != null) {
+                        point.SetPosX((int)event.getX(i));
+                        point.SetPosY((int)event.getY(i));
+                    }
+                }
+
+                int x = (int)event.getX(pointerIndex);
+                int y = (int)event.getY(pointerIndex);
+                map.get(pointerID).SetPosX(x);
+                map.get(pointerID).SetPosY(y);
+            }
+
+            break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_CANCEL:
+                map.remove(pointerID);
+                break;
+        }
+
+
+        invalidate();
+
+        return true;
+    }
+}
