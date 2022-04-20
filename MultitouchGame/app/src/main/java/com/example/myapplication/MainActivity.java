209f3358d7f03112099d39d9139565efb3f17123
jmiguel1963 <svsjmvsti@gmail.com>
2022-04-20 19:35:19 +0200

    counter_button_label
    We have painted circles with a color and a random selected circle with another color. The different cercle will get number 1 and the others numbers higher than1. A button resets final state and a label indicates time remaining to stop touching and selecting circles

--- MultitouchGame/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MultitouchGame/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     MultitouchView myTouchView;
+    ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        myTouchView = new MultitouchView(this);
-        setContentView(myTouchView);
-        //setContentView(R.layout.activity_main);
+        //myTouchView = new MultitouchView();
+        //setContentView(myTouchView);
+
+        setContentView(R.layout.activity_main);
+        myTouchView=findViewById(R.id.mutitouchview);
+        //Log.i("distance",""+this);
+        myTouchView.initView(this);
     }
 }
--- MultitouchGame/app/src/main/java/com/example/myapplication/MultitouchView.java
+++ MultitouchGame/app/src/main/java/com/example/myapplication/MultitouchView.java
@@ -1,87 +1,204 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 
 public class MultitouchView extends View implements View.OnTouchListener{
 
     private Paint paint;
-    private int[] colors={Color.BLACK,Color.BLUE,Color.YELLOW,Color.GREEN,Color.CYAN,Color.RED};
-    private CountDownTimer cTimer = null;
-    private boolean selectedPlayer=false;
+    private Paint paintText;
+    private static final int SIZE_CIRCLE = 200;
+    private static final int SIZE_TEXT = 190;
+    CountDownTimer cTimer = null;
+    private int[] colors = { Color.BLUE, Color.GREEN, Color.MAGENTA,
+            Color.BLACK, Color.CYAN, Color.GRAY, Color.RED, Color.DKGRAY,
+            Color.LTGRAY, Color.YELLOW };
+    private int colorIndex = 0;
 
-    Map <Integer,Vector2D> map =  new HashMap<Integer,Vector2D>();
-    public MultitouchView(Context context) {
-        super(context);
+    ArrayList<Vector2D> map = new ArrayList<Vector2D>();
+    private int playerSelected = -1;
+    TextView lbl_info = null;
+    Button btn_restart = null;
+
+    public MultitouchView(Context context, AttributeSet attrs) {
+        super(context, attrs);
         paint = new Paint();
         paint.setAntiAlias(true);
         paint.setARGB(255,255,0,0);
 
+
+        paintText = new Paint();
+        paintText.setColor(Color.WHITE);
+        paintText.setTextSize(SIZE_TEXT);
+
         this.setOnTouchListener(this);
+    }
+
+    public void initView(MainActivity activity){
+        btn_restart = activity.findViewById(R.id.buttonReset);
+        lbl_info = activity.findViewById(R.id.messages);
+        lbl_info.setText("");
+        btn_restart.setVisibility(INVISIBLE);
+        btn_restart.setOnClickListener(new OnClickListener() {
+            public void onClick(View v)
+            {
+                map.clear();
+                playerSelected = -1;
+                invalidate();
+                btn_restart.setVisibility(INVISIBLE);
+                lbl_info.setText("");
+            }
+        });
     }
 
     @Override
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
-        int index=0;
-        for (Map.Entry<Integer, Vector2D> pair: map.entrySet()) {
-            int x = pair.getValue().GetPosX();
-            int y = pair.getValue().GetPosY();
-            paint.setColor(colors[index%colors.length]);
-            canvas.drawCircle(x,y,100, paint);
-            index++;
+
+        if (playerSelected == -1){
+            for (int i = 0; i < map.size(); i++){
+                int x = map.get(i).posX;
+                int y = map.get(i).posY;
+                int c = map.get(i).color;
+                paint.setColor(c);
+                canvas.drawCircle(x,y,SIZE_CIRCLE, paint);
+
+            }
+        }else{
+            int theOthers = 2;
+            String text = "";
+            for (int i = 0; i < map.size(); i++){
+                int x = map.get(i).posX;
+                int y = map.get(i).posY;
+                if(i == playerSelected){
+                    paint.setColor(Color.RED);
+                    text = "1";
+                }else{
+                    paint.setColor(Color.BLACK);
+                    text = ""+theOthers;
+                    theOthers++;
+                }
+                canvas.drawCircle(x,y,SIZE_CIRCLE, paint);
+                canvas.drawText(text, x - (int)(SIZE_TEXT*0.25),y+(int)(SIZE_TEXT*0.25), paintText);
+            }
         }
+
+
+
+
+
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent event){
-
+        if (playerSelected!= -1){
+            return true;
+        }
         int pointerIndex = event.getActionIndex();
         int pointerID = event.getPointerId(pointerIndex);
-        Log.i("distance",""+pointerID);
         int maskedAction = event.getActionMasked();
-
+        Log.i("touch_enric", ""+ pointerID);
         switch (maskedAction){
             case MotionEvent.ACTION_POINTER_DOWN:
             case MotionEvent.ACTION_DOWN: {
                 int x = (int) event.getX(pointerIndex);
                 int y = (int) event.getY(pointerIndex);
-                Vector2D newPoint = new Vector2D(x, y);
-                map.put(pointerID, newPoint);
+                Vector2D newPoint = new Vector2D(x, y, pointerID, colors[colorIndex% colors.length]);
+                colorIndex++;
+                map.add(newPoint);
+
+                if (map.size()>1){
+                    if (cTimer != null){
+                        cTimer.cancel();
+                    }
+                    lbl_info.setText("Calculating player positions in...3");
+                    cTimer = new CountDownTimer(3000, 1000)
+                    {
+                        public void onTick(long millisUntilFinished) {
+                            int seconds = (int)(millisUntilFinished / 1000);
+                            lbl_info.setText("Calculating player positions in..."+seconds);
+
+                        }
+                        public void onFinish() {
+
+                            btn_restart.setVisibility(VISIBLE);
+                            lbl_info.setText("");
+
+                            int min = 0;
+                            int max = map.size();
+                            Random random = new Random();
+                            playerSelected = random.nextInt(max + min) + min;
+
+                            invalidate();
+                            if (cTimer != null){
+                                cTimer.cancel();
+                            }
+
+                        }
+                    };
+                    cTimer.start();
+                }
+
+
             }
 
             break;
             case MotionEvent.ACTION_MOVE:{
                 int size = event.getPointerCount();
                 for (int i = 0; i < size; i++) {
-                    Vector2D point = map.get(event.getPointerId(i));
-                    if (point != null) {
-                        point.SetPosX((int)event.getX(i));
-                        point.SetPosY((int)event.getY(i));
+
+                    boolean found = false;
+                    int index = 0;
+                    while(!found && index < map.size()) {
+                        if (map.get(index).ID == event.getPointerId(i)) {
+                            map.get(index).posX = (int) event.getX(i);
+                            map.get(index).posY = (int) event.getY(i);
+                            found = true;
+                        }
+                        index++;
                     }
                 }
 
-                int x = (int)event.getX(pointerIndex);
-                int y = (int)event.getY(pointerIndex);
-                map.get(pointerID).SetPosX(x);
-                map.get(pointerID).SetPosY(y);
             }
 
             break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_CANCEL:
-                map.remove(pointerID);
+                lbl_info.setText("");
+                if (cTimer != null){
+                    cTimer.cancel();
+                }
+                boolean found = false;
+                int index = 0;
+                while(!found && index < map.size()){
+                    if (map.get(index).ID == pointerID){
+                        map.remove(index);
+                        found = true;
+                    }
+                    index++;
+                }
                 break;
         }
 
